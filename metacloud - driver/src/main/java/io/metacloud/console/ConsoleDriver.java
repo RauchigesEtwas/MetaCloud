cb43c406cc8d2b14455f89bf90ef4846c0f95c95
ShortCodes <Bohlandrobin.rb@gmail.com>
2022-02-25 19:38:12 +0100

    Merge remote-tracking branch 'origin/Cloud' into Cloud

--- metacloud - driver/src/main/java/io/metacloud/console/ConsoleDriver.java
+++ metacloud - driver/src/main/java/io/metacloud/console/ConsoleDriver.java
@@ -1,8 +1,14 @@
 package io.metacloud.console;
 
+import io.metacloud.command.CommandDriver;
+
 public class ConsoleDriver extends  Thread{
 
+
+    private CommandDriver commandDriver;
+
     public ConsoleDriver(){
+        this.commandDriver = new CommandDriver();
 
     }
 
