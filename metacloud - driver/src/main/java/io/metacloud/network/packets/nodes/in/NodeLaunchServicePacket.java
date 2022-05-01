48a2f3eb40bb3f6b55b0c96c4e96ce3a1ab6f26f
ShortCodes <Bohlandrobin.rb@gmail.com>
2022-05-01 14:22:17 +0200

    Adding service to Proxy

--- metacloud - driver/src/main/java/io/metacloud/network/packets/nodes/in/NodeLaunchServicePacket.java
+++ metacloud - driver/src/main/java/io/metacloud/network/packets/nodes/in/NodeLaunchServicePacket.java
@@ -7,17 +7,40 @@ public class NodeLaunchServicePacket extends Packet {
 
     private String group;
     private Integer serviceID;
+    private boolean sameAddress;
+    private Integer port;
 
     @Override
     public void write(IBuffer buffer) {
         buffer.write("group", group);
         buffer.write("serviceID", serviceID);
+        buffer.write("sameAddress", sameAddress);
+        buffer.write("port", port);
     }
 
     @Override
     public void read(IBuffer buffer) {
         this.group = buffer.read("group", String.class);
         this.serviceID = buffer.read("serviceID", Integer.class);
+        this.sameAddress = buffer.read("sameAddress", Boolean.class);
+        this.port = buffer.read("port", Integer.class);
+    }
+
+
+    public boolean isSameAddress() {
+        return sameAddress;
+    }
+
+    public void setSameAddress(boolean sameAddress) {
+        this.sameAddress = sameAddress;
+    }
+
+    public Integer getPort() {
+        return port;
+    }
+
+    public void setPort(Integer port) {
+        this.port = port;
     }
 
     public String getGroup() {
---
+++ metacloud - driver/src/main/java/io/metacloud/network/packets/services/out/ServiceUnregisterPacket.java
@@ -0,0 +1,28 @@
+package io.metacloud.network.packets.services;
+
+import io.metacloud.protocol.IBuffer;
+import io.metacloud.protocol.Packet;
+
+public class ServiceUnregisterPacket extends Packet {
+
+    private String service;
+
+
+    @Override
+    public void write(IBuffer buffer) {
+        buffer.write("service", service);
+    }
+
+    @Override
+    public void read(IBuffer buffer) {
+        service = buffer.read("service", String.class);
+    }
+
+    public String getService() {
+        return service;
+    }
+
+    public void setService(String service) {
+        this.service = service;
+    }
+}
